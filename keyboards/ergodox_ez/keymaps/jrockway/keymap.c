a45262b4ac00a9bf09be4142d9865c91c91b5c48
Jonathan Rockway <jon@jrock.us>
2018-10-25 09:55:06 -0400

    swap equal and minus

--- keyboards/ergodox_ez/keymaps/jrockway/keymap.c
+++ keyboards/ergodox_ez/keymaps/jrockway/keymap.c
@@ -19,7 +19,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_ergodox(
                        KC_ESCAPE,KC_1,KC_2,KC_3,KC_4,KC_5,TG(FKEYS),
-                       KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_EQUAL,
+                       KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_MINUS,
                        KC_LCTRL,KC_A,KC_S,KC_D,KC_F,KC_G,
                        KC_LSHIFT,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_BSPACE,
                        KC_GRAVE,KC_LGUI,KC_LEFT,KC_LALT,KC_RIGHT,
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_END,
 
                        TG(MEDIA),KC_6,KC_7,KC_8,KC_9,KC_0,KC_BSPACE,
-                       KC_MINUS,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLASH,
+                       KC_EQUAL,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLASH,
                        KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_QUOTE,
                        KC_BSPACE,KC_N,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_SFTENT,
                        KC_DOWN,KC_UP,KC_LBRACKET,KC_RBRACKET,RCTL_T(KC_PSCREEN),

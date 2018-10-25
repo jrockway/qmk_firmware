8c3a3f8fcc3fb5eb844e0b72fa3a4e5259280705
Jonathan Rockway <jon@jrock.us>
2018-10-25 10:22:27 -0400

    tapdance underscore interferes with --

--- keyboards/ergodox_ez/keymaps/jrockway/keymap.c
+++ keyboards/ergodox_ez/keymaps/jrockway/keymap.c
@@ -10,15 +10,14 @@
 
 #define TD_LB 0
 #define TD_RB 1
-#define TD_MINUS 2
 #define TD_EQUAL 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_ergodox(
                        KC_ESCAPE,KC_1,KC_2,KC_3,KC_4,KC_5,TG(FKEYS),
-                       KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,TD(TD_MINUS),
+                       KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_MINUS,
                        KC_LCTRL,KC_A,KC_S,KC_D,KC_F,KC_G,
-                       KC_LSHIFT,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_BSPACE,
+                       KC_LSHIFT,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_UNDERSCORE,
                        KC_GRAVE,KC_LGUI,KC_LEFT,KC_LALT,KC_RIGHT,
                        KC_INS,KC_ESCAPE,
                        KC_HOME,
@@ -107,7 +106,6 @@ const uint16_t PROGMEM fn_actions[] = {
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_LB] = ACTION_TAP_DANCE_DOUBLE(KC_LPRN,KC_LCBR),
   [TD_RB] = ACTION_TAP_DANCE_DOUBLE(KC_RPRN,KC_RCBR),
-  [TD_MINUS] = ACTION_TAP_DANCE_DOUBLE(KC_MINUS,KC_UNDERSCORE),
   [TD_EQUAL] = ACTION_TAP_DANCE_DOUBLE(KC_EQUAL,KC_PLUS),
 };
 

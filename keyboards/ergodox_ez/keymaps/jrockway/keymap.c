5dbfd1f8c39a61c37790b9ec46c4385946d8fc66
Jonathan Rockway <jon@jrock.us>
2018-10-25 10:11:50 -0400

    tap dance _ and + and remove random keycodes we don't use

--- keyboards/ergodox_ez/keymaps/jrockway/keymap.c
+++ keyboards/ergodox_ez/keymaps/jrockway/keymap.c
@@ -8,34 +8,31 @@
 #define MEDIA 2
 #define SYMS 3
 
-enum custom_keycodes {
-  PLACEHOLDER = SAFE_RANGE, // can always be here
-  EPRM,
-  VRSN,
-  RGB_SLD
-};
-
+#define TD_LB 0
+#define TD_RB 1
+#define TD_MINUS 2
+#define TD_EQUAL 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_ergodox(
                        KC_ESCAPE,KC_1,KC_2,KC_3,KC_4,KC_5,TG(FKEYS),
-                       KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_MINUS,
+                       KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,TD(TD_MINUS),
                        KC_LCTRL,KC_A,KC_S,KC_D,KC_F,KC_G,
                        KC_LSHIFT,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_BSPACE,
                        KC_GRAVE,KC_LGUI,KC_LEFT,KC_LALT,KC_RIGHT,
                        KC_INS,KC_ESCAPE,
                        KC_HOME,
-                       KC_SPACE,TD(0), // big thumb keys
+                       KC_SPACE,TD(TD_LB), // big thumb keys
                        KC_END,
 
                        TG(MEDIA),KC_6,KC_7,KC_8,KC_9,KC_0,KC_BSPACE,
-                       KC_EQUAL,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLASH,
+                       TD(TD_EQUAL),KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLASH,
                        KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_QUOTE,
                        KC_BSPACE,KC_N,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_SFTENT,
                        KC_DOWN,KC_UP,KC_LBRACKET,KC_RBRACKET,RCTL_T(KC_PSCREEN),
                        KC_ESCAPE,KC_DEL,
                        KC_PGUP,KC_PGDOWN,
-                       TD(1),KC_SPACE), // big thumb keys
+                       TD(TD_RB),KC_SPACE), // big thumb keys
 
   [FKEYS] = LAYOUT_ergodox(
                        _______,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,_______,
@@ -108,54 +105,11 @@ const uint16_t PROGMEM fn_actions[] = {
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [0] = ACTION_TAP_DANCE_DOUBLE(KC_LPRN,KC_LCBR),
-  [1] = ACTION_TAP_DANCE_DOUBLE(KC_RPRN,KC_RCBR),
+  [TD_LB] = ACTION_TAP_DANCE_DOUBLE(KC_LPRN,KC_LCBR),
+  [TD_RB] = ACTION_TAP_DANCE_DOUBLE(KC_RPRN,KC_RCBR),
+  [TD_MINUS] = ACTION_TAP_DANCE_DOUBLE(KC_MINUS,KC_UNDERSCORE),
+  [TD_EQUAL] = ACTION_TAP_DANCE_DOUBLE(KC_EQUAL,KC_PLUS),
 };
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-  switch(id) {
-    case 0:
-      if (record->event.pressed) {
-        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-      }
-      break;
-    case 1:
-      if (record->event.pressed) { // For resetting EEPROM
-        eeconfig_init();
-      }
-      break;
-  }
-  return MACRO_NONE;
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    // dynamically generate these.
-    case EPRM:
-      if (record->event.pressed) {
-        eeconfig_init();
-      }
-      return false;
-      break;
-    case VRSN:
-      if (record->event.pressed) {
-        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-      }
-      return false;
-      break;
-    case RGB_SLD:
-      if (record->event.pressed) {
-        #ifdef RGBLIGHT_ENABLE
-          rgblight_mode(1);
-        #endif
-      }
-      return false;
-      break;
-  }
-  return true;
-}
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {

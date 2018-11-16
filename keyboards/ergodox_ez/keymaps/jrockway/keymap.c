7ad9eb0591e8d4abee94025cccdb7a34db7f1933
Jonathan Rockway <jrockway@pilotfiber.com>
2018-11-16 16:09:11 -0500

    more symmetry

--- keyboards/ergodox_ez/keymaps/jrockway/keymap.c
+++ keyboards/ergodox_ez/keymaps/jrockway/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_MINUS,
                        KC_LCTRL,KC_A,KC_S,KC_D,KC_F,KC_G,
                        KC_LSHIFT,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_UNDERSCORE,
-                       KC_LGUI,KC_GRAVE,MO(RAISE),MO(RAISE),MO(RAISE),
+                       KC_LGUI,KC_GRAVE,KC_DEL,MO(RAISE),MO(RAISE),
                        KC_DEL,KC_INS,
                        KC_PGUP,
                        KC_LALT,KC_SPACE, // big thumb keys
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_EQUAL,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLASH,
                        KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_QUOTE,
                        KC_BSPACE,KC_N,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_RSHIFT,
-                       MO(LOWER),KC_PLUS,KC_LBRACKET,KC_RBRACKET,RCTL_T(KC_PSCREEN),
+                       MO(LOWER),MO(LOWER),KC_PLUS,KC_PSCREEN,KC_RCTL,
                        KC_LEFT,KC_RIGHT,
                        KC_UP,KC_DOWN,
                        KC_ENTER,KC_SPACE), // big thumb keys

f64a1df614e97259f99d5208f3942252ebe28749
Jonathan Rockway <jrockway@users.noreply.github.com>
2020-01-11 07:32:27 -0500

    numpad: very misleading indentation caused 0 to be in the wrong place; fix

--- keyboards/ergodox_ez/keymaps/jrockway/keymap.c
+++ keyboards/ergodox_ez/keymaps/jrockway/keymap.c
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        _______, _______,   KC_KP_7,    KC_KP_8,     KC_KP_9,        KC_KP_PLUS,_______,
                                 KC_BSPACE, KC_KP_4,    KC_KP_5,     KC_KP_6,        KC_KP_PLUS,_______,
                        _______, KC_BSPACE, KC_KP_1,    KC_KP_2,     KC_KP_3,        KC_KP_ENTER,_______,
-                                  _______, _______,    KC_KP_0,     KC_KP_DOT,      KC_KP_ENTER,
+                                           _______,    KC_KP_0,     KC_KP_DOT,      KC_KP_ENTER,_______,
                        _______,_______,
                        _______,_______,
                        _______,_______), // big
